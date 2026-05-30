fa4cd2ebabc6809d4b3df0990a81df3a53b71105
PrimeEmre <ynsemrgzl@gmail.com>
2026-05-29 23:57:57 -0400

    initial

---
+++ site/search.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script type="text/javascript" src="js/jquery-migrate-1.1.1.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+    <script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
+    <!--[if lt IE 8]>
+        <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header>
+  <div class="logotip">
+    <div class="container">
+      <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt=""></a><span>European restaurant</span></h1>   
+      <div class="text-header">
+        The right place to have a good<br> rest!
+      </div>
+    </div>
+  </div>
+   <div class="container">
+         <div class="navbar navbar_ clearfix">
+            <div class="navbar-inner">   
+                  
+                  <div class="clearfix"> 
+                    <ul class="soc-icons">
+                      <li><a href="#" class="icon"></a></li>
+                      <li><a href="#" class="icon-1"></a></li>
+                      <li><a href="#" class="icon-2"></a></li>
+                    </ul>
+                    <div class="nav-collapse nav-collapse_ collapse">
+                        <ul class="nav sf-menu clearfix">
+                          <li><a href="index.html">Home</a></li>
+                          <li class="sub-menu "><a href="index-1.html">About</a>
+                             <ul>
+                              <li><a href="#">Dessert</a></li>
+                              <li><a href="#">Dinner</a></li>
+                              <li class="sub-menu-1"><a href="#">Salad</a>
+                                <ul>
+                                  <li><a href="#">Beef</a></li>
+                                  <li><a href="#">Chicken</a></li>
+                                  <li><a href="#">Eggs</a></li>
+                                </ul>
+                              </li>
+                             </ul>
+                          </li>
+                          <li><a href="index-2.html">Cuisine</a></li>
+                          <li><a href="index-3.html">Events</a></li>
+                          <li><a href="index-4.html">Contacts</a></li>
+                        </ul>
+                    </div>
+
+                  </div>
+             </div>  
+         </div>
+    </div> 
+</header>
+<div id="content">
+  <div class="container"> 
+    <div class="row">
+      <div class="span12">
+       <h2 class="h2-pad">Search result:</h2>
+        <div id="search-results"></div>
+      </div>
+    </div>
+  </div>
+</div>
+<footer>
+  <div class="container">
+        <div class="row"><div class="span12"></div></div>
+         <div class="row">
+           <article class="span5 fright">
+             <ul class="list-3">
+               <li><a href="#">Your Account</a></li>
+               <li><a href="#">Personal information</a></li>
+               <li><a href="#">Addresses</a></li>
+             </ul>
+           </article>
+           <article class="span2 fright">
+             <ul class="list-3">
+               <li><a href="#">New products</a></li>
+               <li><a href="#">Top sellers</a></li>
+               <li><a href="#">Specials</a></li>
+             </ul>
+           </article> 
+           <article class="span2 fright">
+             <ul class="list-3">
+               <li><a href="#">Returns</a></li>
+               <li><a href="#">Delivery</a></li>
+               <li><a href="#">Customer Service</a></li>
+             </ul>
+           </article>
+           <article class="span3 fleft">
+                 <span><a href="index.html"><img src="img/logo-1.png" alt=""></a></span> <em>&copy; 2013 &nbsp;&nbsp;<br class="br-footer"><a href="index-5.html">Privacy Policy</a></em>
+           </article>
+       </div>
+  </div>
+</footer>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
